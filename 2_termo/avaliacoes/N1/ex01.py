5d524596ca58ffdf55806b09b999ec32840d5ee5
Diogo Costa <diogo.henrique.cdl@gmail.com>
2023-04-13 20:38:15 -0300

    Alteracoes ex01.py >> N1

--- 2_termo/avaliacoes/N1/ex01.py
+++ 2_termo/avaliacoes/N1/ex01.py
@@ -1,3 +1,16 @@
+"""
+Faça um programa em Pythoncom uma função que receba como parâmetro uma string e imprima os caracteres repetidos contidos na string.
+
+Ex.: 
+Entrada: Renata  Saída: a a
+Entrada: arvore  Saída: r r
+Entrada: carrossel  Saída: r r s s
+Entrada: estante  Saída: e t t e
+Entrada: brasileira  Saída: r a i i r a
+
+No programa principal, peçaa stringechame a função.
+"""
+
 def repet(stri):
     sai = " "
     st = list(stri)
