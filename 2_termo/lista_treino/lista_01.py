9cd93f6ce286eb86ce745be4532b194b873814e2
Diogo Costa <diogo.henrique.cdl@gmail.com>
2023-04-26 22:55:36 -0300

    Add lista_01.py >> lista_treino (2_termo)

---
+++ 2_termo/lista_treino/lista_01.py
@@ -0,0 +1,71 @@
+# >>>> VARIAVEIS E EXPRESSOES <<<<
+
+"""
+1. Faça um programa que leia um numero inteiro e o imprima. ´
+"""
+
+
+num = int(input())
+print(num)
+
+
+# ------------------------------
+
+"""
+2. Faça um programa que leia um numero real e o imprima. ´
+"""
+
+
+num = float(input())
+print(num)
+
+
+# ------------------------------
+
+"""
+3. Peça ao usuario para digitar tres valores inteiros e imprima a soma deles.
+"""
+
+
+print("Insira tres valores inteiros: ")
+n1 = int(input())
+n2 = int(input())
+n3 = int(input())
+print(f"Soma = {n1+n2+n3}")
+
+
+# ------------------------------
+
+"""
+4. Leia um numero real e imprima o resultado do quadrado desse numero.
+"""
+
+
+num = float(input())
+print(num**2)
+
+
+# ------------------------------
+
+"""
+5. Leia um numero real e imprima a quinta parte deste numero.
+"""
+
+
+num = float(input())
+print(num/5)
+
+
+# ------------------------------
+
+"""
+6. Leia uma temperatura em graus Celsius e apresente-a convertida em graus Fahrenheit.
+A formula de conversao e: F = C*(9.0/5.0)+32.0, sendo F a temperatura em Fahrenheit
+e C a temperatura em Celsius.
+"""
+
+"""
+Codigo
+"""
+
+# ------------------------------
