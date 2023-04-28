65d914754755403ec5bd652ee730465f33397e6d
Diogo Costa <diogo.henrique.cdl@gmail.com>
2023-04-27 21:52:25 -0300

    Add exer 6 -> 10 (lista_treino)

--- 2_termo/lista_treino/lista_01.py
+++ 2_termo/lista_treino/lista_01.py
@@ -1,24 +1,24 @@
 # >>>> VARIAVEIS E EXPRESSOES <<<<
 
 """
-1. Faça um programa que leia um numero inteiro e o imprima. ´
+1. Faça um programa que leia um numero inteiro e o imprima.
 """
 
-
+"""
 num = int(input())
 print(num)
-
+"""
 
 # ------------------------------
 
 """
-2. Faça um programa que leia um numero real e o imprima. ´
+2. Faça um programa que leia um numero real e o imprima.
 """
 
-
+"""
 num = float(input())
 print(num)
-
+"""
 
 # ------------------------------
 
@@ -26,13 +26,13 @@ print(num)
 3. Peça ao usuario para digitar tres valores inteiros e imprima a soma deles.
 """
 
-
+"""
 print("Insira tres valores inteiros: ")
 n1 = int(input())
 n2 = int(input())
 n3 = int(input())
 print(f"Soma = {n1+n2+n3}")
-
+"""
 
 # ------------------------------
 
@@ -40,10 +40,10 @@ print(f"Soma = {n1+n2+n3}")
 4. Leia um numero real e imprima o resultado do quadrado desse numero.
 """
 
-
+"""
 num = float(input())
 print(num**2)
-
+"""
 
 # ------------------------------
 
@@ -51,21 +51,79 @@ print(num**2)
 5. Leia um numero real e imprima a quinta parte deste numero.
 """
 
-
+"""
 num = float(input())
 print(num/5)
-
+"""
 
 # ------------------------------
 
 """
 6. Leia uma temperatura em graus Celsius e apresente-a convertida em graus Fahrenheit.
-A formula de conversao e: F = C*(9.0/5.0)+32.0, sendo F a temperatura em Fahrenheit
+A formula de conversao e: F = C * (9.0/5.0)+32.0, sendo F a temperatura em Fahrenheit
 e C a temperatura em Celsius.
 """
 
 """
-Codigo
+cel = float(input())
+fah = cel * (9.0 / 5.0) + 32.0
+print(f"Celsius > {cel}º\nFahrenheit > {fah:.1f}º")
+"""
+
+# ------------------------------
+
+"""
+7. Leia uma temperatura em graus Fahrenheit e apresente-a convertida em graus Celsius.
+A formula de conversao e: C = 5.0 * (F - 32.0)/9.0, sendo C a temperatura em Celsius
+e F a temperatura em Fahrenheit.
+"""
+
+"""
+fah = float(input())
+cel = 5.0 * (fah - 32.0) / 9.0
+print(f"Fahrenheit > {fah}º\nCelsius > {cel:.1f}º")
+"""
+
+# ------------------------------
+
+"""
+8. Leia uma temperatura em graus Kelvin e apresente-a convertida em graus Celsius. A
+formula de conversaoe: C = K - 273.15, sendo C a temperatura em Celsius e K a
+temperatura em Kelvin.
+"""
+
+"""
+kel = float(input())
+cel = kel - 273.15
+print(f"Kelvin > {kel}º\nCelsius > {cel:.2f}º")
+"""
+
+# ------------------------------
+
+"""
+9. Leia uma temperatura em graus Celsius e apresente-a convertida em graus Kelvin. A
+formula de conversao e: K = C + 273.15, sendo C a temperatura em Celsius e K a
+temperatura em Kelvin.
+"""
+
+"""
+cel = float(input())
+kel = cel + 273.15
+print(f"Celsius > {cel}º\nKelvin > {kel:.2f}º")
+"""
+
+# ------------------------------
+
+"""
+10. Leia uma velocidade em km/h (quilometros por hora) e apresente-a convertida em m/s
+(metros por segundo). A formula de conversao e: M = K/3.6, sendo K a velocidade em
+km/h e M em m/s.
+"""
+
+"""
+km = float(input())
+ms = km / 3.6
+print(f"Km/h > {km}\nM/s > {ms:.6f}")
 """
 
 # ------------------------------
