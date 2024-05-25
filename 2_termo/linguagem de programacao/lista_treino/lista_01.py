efa7682a36359d2caee3e64b0c4e53458d985fe3
Diogo Lima <diogo.henrique.cdl@gmail.com>
2024-05-25 14:44:59 -0300

    Dir lista_treino [17 exer]

---
+++ 2_termo/linguagem de programacao/lista_treino/lista_01.py
@@ -0,0 +1,207 @@
+# >>>> VARIAVEIS E EXPRESSOES <<<<
+
+"""
+1. Faça um programa que leia um numero inteiro e o imprima.
+"""
+
+"""
+num = int(input())
+print(num)
+"""
+
+# ------------------------------
+
+"""
+2. Faça um programa que leia um numero real e o imprima.
+"""
+
+"""
+num = float(input())
+print(num)
+"""
+
+# ------------------------------
+
+"""
+3. Peça ao usuario para digitar tres valores inteiros e imprima a soma deles.
+"""
+
+"""
+print("Insira tres valores inteiros: ")
+n1 = int(input())
+n2 = int(input())
+n3 = int(input())
+print(f"Soma = {n1+n2+n3}")
+"""
+
+# ------------------------------
+
+"""
+4. Leia um numero real e imprima o resultado do quadrado desse numero.
+"""
+
+"""
+num = float(input())
+print(num**2)
+"""
+
+# ------------------------------
+
+"""
+5. Leia um numero real e imprima a quinta parte deste numero.
+"""
+
+"""
+num = float(input())
+print(num/5)
+"""
+
+# ------------------------------
+
+"""
+6. Leia uma temperatura em graus Celsius e apresente-a convertida em graus Fahrenheit.
+A formula de conversao e: F = C * (9.0/5.0)+32.0, sendo F a temperatura em Fahrenheit
+e C a temperatura em Celsius.
+"""
+
+"""
+cel = float(input())
+fah = cel * (9.0 / 5.0) + 32.0
+print(f"Celsius > {cel}º\nFahrenheit > {fah:.1f}º")
+"""
+
+# ------------------------------
+
+"""
+7. Leia uma temperatura em graus Fahrenheit e apresente-a convertida em graus Celsius.
+A formula de conversao e: C = 5.0 * (F - 32.0)/9.0, sendo C a temperatura em Celsius
+e F a temperatura em Fahrenheit.
+"""
+
+"""
+fah = float(input())
+cel = 5.0 * (fah - 32.0) / 9.0
+print(f"Fahrenheit > {fah}º\nCelsius > {cel:.1f}º")
+"""
+
+# ------------------------------
+
+"""
+8. Leia uma temperatura em graus Kelvin e apresente-a convertida em graus Celsius. A
+formula de conversaoe: C = K - 273.15, sendo C a temperatura em Celsius e K a
+temperatura em Kelvin.
+"""
+
+"""
+kel = float(input())
+cel = kel - 273.15
+print(f"Kelvin > {kel}º\nCelsius > {cel:.2f}º")
+"""
+
+# ------------------------------
+
+"""
+9. Leia uma temperatura em graus Celsius e apresente-a convertida em graus Kelvin. A
+formula de conversao e: K = C + 273.15, sendo C a temperatura em Celsius e K a
+temperatura em Kelvin.
+"""
+
+"""
+cel = float(input())
+kel = cel + 273.15
+print(f"Celsius > {cel}º\nKelvin > {kel:.2f}º")
+"""
+
+# ------------------------------
+
+"""
+10. Leia uma velocidade em km/h (quilometros por hora) e apresente-a convertida em m/s
+(metros por segundo). A formula de conversao e: M = K/3.6, sendo K a velocidade em
+km/h e M em m/s.
+"""
+
+"""
+km = float(input())
+ms = km / 3.6
+print(f"Km/h > {km}\nM/s > {ms:.6f}")
+"""
+
+# ------------------------------
+
+"""
+11.  Leia uma velocidade em m/s (metros por segundo) e apresente-a convertida em km/h
+(quilometros por hora). A formula de conversao e: K = M * 3.6, sendo K a velocidade
+em km/h e M em m/s.
+"""
+
+"""
+vms = float(input())
+print(f"{vms} m/s e igual a {vms * 3.6} km/h")
+"""
+
+# ------------------------------
+
+"""
+12. Leia uma distancia em milhas e apresente-a convertida em quilometros. A formula de 
+conversao e: K = 1, 61 * M, sendo K a distancia em quilometros e M em milhas.
+"""
+
+"""
+mil = float(input())
+print(f"{mil} milhas e igual a {1.61 * mil} quilometros")
+"""
+
+# ------------------------------
+
+"""
+13. Leia uma distancia em quilometros e apresente-a convertida em milhas. A formula de
+conversao e: M = K / 1,61, sendo K a distancia em quilometros e M em milhas.
+"""
+
+"""
+k = float(input())
+print(f"{k} quilometros e igual a {k/1.61:.2f} milhas")
+"""
+
+# ------------------------------
+
+"""
+14. Leia um angulo em graus e apresente-o convertido em radianos. A formula de conversao
+e: R = G * π/180, sendo G o angulo em graus e R em radianos e π = 3.14.
+"""
+
+"""
+ang = float(input())
+print(f"{ang} graus e igual a {ang * 3.14 / 180:.7f} radianos")
+"""
+
+# ------------------------------
+
+"""
+15. Leia um angulo em radianos e apresente-o convertido em graus. A formula de conversao
+e: G = R * 180/π, sendo G o angulo em graus e R em radianos e π = 3.14.
+"""
+
+"""
+r = float(input())
+print(f"{r} radianos e igual a {r * 180 / 3.14} graus")
+"""
+
+# ------------------------------
+
+"""
+16. Leia um valor de comprimento em polegadas e apresente-o convertido em centimetros.
+A formula de conversao e: C = P * 2.54, sendo C o comprimento em centimetros e P o
+comprimento em polegadas.
+"""
+
+"""
+p = float(input())
+print(f"{p} polegadas e igual a {p * 2.54} centimetros")
+"""
+
+# ------------------------------
+
+"""
+17. 
+"""
